9f49b84d98cab028080889481b5500d76d604948
fei.xia <fei.xia@generalbiologic.com>
2018-06-07 08:59:58 +0800

    git attribute

---
+++ Word/WordProcess/WordProcess/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Words;
+
+namespace WordProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string path = @"C:\Users\fei.xia\Desktop\111111\要处理的备份\cwgl_lx0201\cwgl_lx0202.doc";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document(path);
+            var nodes = doc.ChildNodes;
+        }
+    }
+}
